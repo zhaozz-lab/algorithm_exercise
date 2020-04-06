922ff73dcb4beec57c73ec0fc1d5e128ba60a591
zhaozz-lab <18046506891@163.com>
2020-04-06 14:34:22 +0800

    list iteration and reverse

--- list.py
+++ list.py
@@ -21,7 +21,7 @@ def traverse(head):
     while temp.next is not None:
         print(temp.val)
         temp = temp.next
-traverse(temp)
+# traverse(temp)
 
 def reverse(head):
     if head.next is None:
@@ -30,21 +30,25 @@ def reverse(head):
     head.next.next = head
     head.next = None
     return last
-test1 = reverse(head)
-print("the reverse list is ")
-traverse(test1)
+# test1 = reverse(temp)
+# print("the reverse list is ")
+# traverse(test1)
 def reverse_iter(head):
-    newlist = node(-1)
+    newlist = None
     while head is not None:
         nextnode = head.next
-        head.next = newlist.next
-        newlist.next = head
+        head.next = newlist
+        newlist = head
+        
         head = nextnode
-    return newlist.next
-test1 = reverse_iter(head)
+    return newlist
+temp = head
+print(head.next.val)  
+# test1 = reverse_iter(temp)
+test1 = reverse(temp)
 print("the reverse_iter list is ")
 traverse(test1)
-
+print("the result is ")
 
 
     
