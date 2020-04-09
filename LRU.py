aa2ab4168742df510f86b4ad5ccdad01af5f5463
zhaozz-lab <18046506891@163.com>
2020-04-10 07:14:47 +0800

    LRU

--- LRU.py
+++ LRU.py
@@ -49,3 +49,40 @@ class LRUCache(object):
                 del self.map[key]
         cache.addFirst(x)
         self.map.put(key,x)
+
+class LRUCache1:
+ 
+    # @param capacity, an integer
+    def __init__(self, capacity):
+        self.cache = {}
+        self.used_list = []
+        self.capacity = capacity
+ 
+    # @return an integer
+    def get(self, key):
+        if key in self.cache:
+            if key != self.used_list[-1]:
+                self.used_list.remove(key)
+                self.used_list.append(key)
+            return self.cache[key]
+        else:
+            return -1
+ 
+    # @param key, an integer
+    # @param value, an integer
+    # @return nothing
+    def set(self, key, value):
+        if key in self.cache:
+            self.used_list.remove(key)
+        elif len(self.cache) == self.capacity:
+            self.cache.pop(self.used_list.pop(0))
+        self.used_list.append(key)
+        self.cache[key] = value
+
+if __name__ == '__main__':
+    test = LRUCache1(3)
+    test.set(1,3)
+    test.set(2,2)
+    test.set(3,3)
+    test.set(4,3)
+    print(test.get(1))
