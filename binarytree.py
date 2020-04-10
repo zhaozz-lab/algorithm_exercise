2ee04d2fde2589ce3b6ad852249a11fd776f06a8
zhaozz-lab <18046506891@163.com>
2020-04-11 07:47:43 +0800

    add the isSameTree

--- binarytree.py
+++ binarytree.py
@@ -20,45 +20,55 @@ class Tree(object):
         else:
             queue = []
             queue.append(self.root)
-            print(queue)
+            # print(queue)
             while queue:
                 cur = queue.pop(0)
                 if cur.left is None:
                     cur.left = node
+                    return
                 elif cur.right is None:
                     cur.right = node
+                    return
                 else:
                     queue.append(cur.left)
                     queue.append(cur.right)
 
-        def trvaverse(self):
-            if self.root is None:
-                return
-            print(self.root.val)
-            self.traverse(self.left)
-            self.traverse(self.right)
+    # def traverse(self,root):
+    #     if root is None:
+    #         return
+    #     print(root.val)
+    #     self.traverse(root.left)
+    #     self.traverse(root.right)
         
+def traverse(root):
+    if root is None:
+        return
+    print(root.val)
+    traverse(root.left)
+    traverse(root.right)
 
-def isSameTree(tree1,tree2):
-    if tree1 is None and tree2 is None:
+
+def isSameTree(root1,root2):
+    if root1 is None and root2 is None:
         return True
-    if tree1 is None or tree2 is None:
+    if root1 is None or root2 is None:
         return False
-    if tree1.val != tree2.val:
+    if root1.val != root2.val:
         return False
-    return isSameTree(tree1.left,tree2.left) and isSameTree(tree1.right,tree2.right)
+    return isSameTree(root1.left,root2.left) and isSameTree(root1.right,root2.right)
 
 
 if __name__ == '__main__':
     # testbinarytree = binarytree(10,binarytree(20),binarytree(30))
     # testbinarytree.traverse(testbinarytree)
     tree1 = Tree()
-    tree1.add(100)
-    tree1.traverse()
+    # tree1.add(100)
+    # tree1.traverse()
     for i in range(0,10):
        tree1.add(i)
     # test traverse   
-    tree1.trvaverse()
+    traverse(tree1.root)
     # test isSameTree
-    # tree2 = tree1
-    # print(isSameTree(tree1,tree2))
+    tree2 = tree1
+    traverse(tree2.root)
+    print(isSameTree(tree1.root,tree2.root))
