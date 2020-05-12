754658931e42ef2e13be0ace655a0288d3288eab
zhaozz-lab <18046506891@163.com>
2020-05-13 07:12:17 +0800

    floodfill

--- floodfill.py
+++ floodfill.py
@@ -14,17 +14,22 @@ def floodFill(image,row,col,newColor):
 def inArea(image,x,y):
     return x>=0 and x < len(image) and y >=0 and y <len(image[0])
 
-def fill(image,x,y,origColor,newColor)：
+def fill(image,x,y,origColor,newColor):
     if not inArea(image,x,y):
         return
+    if image[x][y] != origColor:
+        return
+
+    image[x][y] = -1
+    fill(image,x,y+1,origColor,newColor)
+    fill(image,x,y-1,origColor,newColor)
+    fill(image,x+1,y,origColor,newColor)
+    fill(image,x-1,y,origColor,newColor)
+    image[x][y] = newColor
     
 
 
 if __name__ == '__main__':
-    # nums = [i for i in range(10,0,-1)]
-    # print(nums)
-    # print(subarraysum(nums,9))
-    num1 = '12'
-    num2 = '32'
-    result = multiply(num1,num2)
-    print(result)
+    image = [[1,1,1],[1,1,0],[1,1,2]]
+    image1 = floodFill(image,1,1,3)
+    print(image1)
