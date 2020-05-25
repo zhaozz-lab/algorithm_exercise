8b3d59f3891beb94ee8c0488bae38197f8ca6f61
zhaozhizhong <zhaozz@7invensun.com>
2020-05-25 22:04:03 +0800

    trap

--- trappingrainwater.py
+++ trappingrainwater.py
@@ -1,31 +1,31 @@
-def fill(x,y):
-    fill(x-1,y)
-    fill(x+1,y)
-    fill(x,y-1)
-    fill(x,y+1)
+# give a array,judge how much water could be filled
+def trap(height):
+    n = len(height)
+    ans = 0
+    for i in range(1,n-1):
+        l_max,r_max = 0,0
+        for j in range(i,n):
+            r_max = max(r_max,height[j])
+        for j in range(0,i+1):
+            l_max = max(l_max,height[j])
+
+        ans += min(l_max,r_max) - height[i]
+
+    return ans
 
 
-def floodFill(image,row,col,newColor):
-    origColor = image[row][col]
-    fill(image,row,col,origColor,newColor)
-    return image
+def trap_memo(height):
+    if len(height) < 0:
+        return 0
+     n = len(height)
+     ans = 0 
+     l_max,r_max = [0]*n,[0]*n
+     l_max[0] = height[0]
+     r_max[n-1] = height[n-1]
 
+     for i in range(1,n):
+        l_max[i] = max()
 
-def inArea(image,x,y):
-    return x>=0 and x < len(image) and y >=0 and y <len(image[0])
-
-def fill(image,x,y,origColor,newColor):
-    if not inArea(image,x,y):
-        return
-    if image[x][y] != origColor:
-        return
-
-    image[x][y] = -1
-    fill(image,x,y+1,origColor,newColor)
-    fill(image,x,y-1,origColor,newColor)
-    fill(image,x+1,y,origColor,newColor)
-    fill(image,x-1,y,origColor,newColor)
-    image[x][y] = newColor
     
 
 
