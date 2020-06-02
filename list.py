513c4c981347aa8eecb0b567f8476663dbf84aeb
zhaozhizhong <zhaozz@7invensun.com>
2020-06-02 09:44:05 +0800

    reverse K

--- list.py
+++ list.py
@@ -4,8 +4,6 @@ class node(object):
         self.val = value
         self.next = node
 
-
-
 head = node(1,None)
 temp = head
 i=0
@@ -21,7 +19,8 @@ def traverse(head):
     while temp.next is not None:
         print(temp.val)
         temp = temp.next
-# traverse(temp)
+
+traverse(temp)
 
 def reverse(head):
     if head.next is None:
@@ -42,14 +41,45 @@ def reverse_iter(head):
         
         head = nextnode
     return newlist
-temp = head
-print(head.next.val)  
+# temp = head
+# print(head.next.val)  
 # test1 = reverse_iter(temp)
-test1 = reverse(temp)
-print("the reverse_iter list is ")
-traverse(test1)
-print("the result is ")
+# test1 = reverse(temp)
+# print("the reverse_iter list is ")
+# traverse(test1)
+# print("the result is ")
+
+def reverse_k(a,b):
+    if a.next is None:
+        return a
+    newlist = None
+    print(a.val)
+    while a != b:
+        nextnode = a.next
+        a.next =  newlist
+        newlist = a
+        a = nextnode
+    return newlist
+
+def reverseKGroup(head,k):
+    if head is None:
+        return None
+    a,b = head,head
+    for i in range(0,k):
+        if b is None:
+            return head
+        b = b.next
+
+    newHead = reverse_k(a,b)
+    a.next = reverseKGroup(b,k)
+    return newHead
+
+traverse(temp)    
+test2 = reverseKGroup(temp,2)
+traverse(test2)
 
 
-    
+# reverse 
+if __name__ == '__main__':
+    pass
                    
